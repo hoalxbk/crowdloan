8da9336b0a4a0bd1234612fb126cd1ae039069e0
thanh.nguyen <thanh.nguyen3@sotatek.com>
2021-04-05 11:42:14 +0700

    add-layout-default

--- frontend-user/src/routes.tsx
+++ frontend-user/src/routes.tsx
@@ -20,7 +20,7 @@ import InvestorForgotPassword from "./pages/ForgotPassword/InvestorForgotPasswor
 import InvestorResetPassword from "./pages/ResetPassword/InvestorResetPassword";
 import InvestorLogin from "./pages/Login/InvestorLogin";
 
-import Account from "./pages/Account";
+// import Account from "./pages/Account";
 
 //@ts-ignore
 import { NotificationContainer, NotificationManager } from 'react-notifications'
@@ -65,7 +65,7 @@ const Routes: React.FC<RouteComponentProps> = (props: RouteComponentProps) => {
         <Route path={'/confirm-email/:role?/:token'} component={ConfirmEmail} />
         <Route path={'/network-change'} component={NetworkChange} />
         <Route path={'/change-password/:role?'} component={ChangePassword} />
-        <Route path={'/account'} component={Account} />
+        {/* <Route path={'/account'} component={Account} /> */}
 
         <Route path={'/coming-soon'} component={ComingSoon} />
         <Route component={NotFoundPage} />
