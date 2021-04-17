9de70dd6d194e6484e61cac659abff09eba20a7c
Nguyen Toan Thang <thang.nguyen2@sotatek.com>
2021-04-17 16:50:53 +0700

    Check signature

--- backend/app/Middleware/CheckSignature.js
+++ backend/app/Middleware/CheckSignature.js
@@ -17,17 +17,21 @@ class CheckSignature {
       const headers = request.headers();
       const signature = params.signature;
       const message = headers.msgsignature;
+
       console.log('Check Signature with: ', params);
       console.log('Message: ', message);
 
       let recover = await web3.eth.accounts.recover(message, signature);
       const recoverConvert = Web3.utils.toChecksumAddress(recover);
       const wallet_address = Web3.utils.toChecksumAddress(params.wallet_address);
-      console.log('recoverConvert: ', recoverConvert, wallet_address);
+      console.log('recoverConvert: ', recover, recoverConvert, wallet_address);
 
       if (recoverConvert && recoverConvert !== wallet_address) {
         throw new ForbiddenException('Invalid signature!');
       }
+
+      headers.wallet_address = wallet_address;
+
       await next();
     } catch (e) {
       console.log('ERROR: ', e);
