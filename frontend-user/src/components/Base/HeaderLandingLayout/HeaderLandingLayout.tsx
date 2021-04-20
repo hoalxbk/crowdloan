73848c79f4fb522e19135256f1e82fbaf9496990
thanh.nguyen <thanh.nguyen3@sotatek.com>
2021-04-20 15:05:57 +0700

    develop

--- frontend-user/src/components/Base/HeaderLandingLayout/HeaderLandingLayout.tsx
+++ frontend-user/src/components/Base/HeaderLandingLayout/HeaderLandingLayout.tsx
@@ -1,10 +1,12 @@
 import useStyles from './styles';
+import { useState } from 'react'
 
 const BrightStartIcon = "/images/bright-star.svg";
 const logo = '/images/logo-red-kite.svg';
 
 const HeaderDefaultLayout = () => {
   const styles = useStyles();
+  const [showBanner, setShowBanner] = useState(true)
 
   return (
     <>
@@ -22,6 +24,18 @@ const HeaderDefaultLayout = () => {
           <a href="#"><i className="custom-icon-facebook"></i></a>
           <a href="#"><i className="custom-icon-github"></i></a>
         </div>
+        {showBanner && <div className={styles.banner}>
+          <img src="/images/icons/ring.svg" alt="red-warning icon" />
+          <span className={styles.loginErrorBannerText}>
+          &nbsp;&nbsp;The first IDO will start on the 1st half of May. Subscribe PolkaFoundry Telegram for the latest updates.
+          &nbsp;&nbsp;<button 
+            className={styles.btnChangeAppNetwork}
+            onClick={() => {window.open('https://t.me/PolkaFoundryANN', '_blank')}}
+          >
+            Change App Network
+          </button>
+          </span>
+        </div>}
       </div>
     </>
   );
