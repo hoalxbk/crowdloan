362ec5147ca2a27f8b6587712beb90509c7faf5f
Nguyen Toan Thang <thang.nguyen2@sotatek.com>
2021-05-05 22:57:36 +0700

    Add default `min_percent_claim`, `max_percent_claim `

--- backend/database/migrations/1620013937366_campaign_claim_config_schema.js
+++ backend/database/migrations/1620013937366_campaign_claim_config_schema.js
@@ -7,8 +7,8 @@ class CampaignClaimConfigSchema extends Schema {
   up () {
     this.create('campaign_claim_config', (table) => {
       table.increments();
-      table.decimal('min_percent_claim', 5, 2).notNullable();
-      table.decimal('max_percent_claim', 5, 2).notNullable();
+      table.decimal('min_percent_claim', 5, 2).notNullable().default(0);
+      table.decimal('max_percent_claim', 5, 2).notNullable().default(100);
       table.string('start_time').nullable();
       table.string('end_time').nullable();
       table.integer('campaign_id').unsigned().notNullable();
