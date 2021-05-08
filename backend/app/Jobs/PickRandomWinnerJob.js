1d76d53641e00fbd28261a8da1c20c7c3fe27630
Nguyen_Viet_Hung <hung.nguyen4@sotatek.com>
2021-05-09 06:45:27 +0700

    - Fix bug logic pickup winner

--- backend/app/Jobs/PickRandomWinnerJob.js
+++ backend/app/Jobs/PickRandomWinnerJob.js
@@ -74,7 +74,8 @@ class PickRandomWinnerJob {
         const pkfBalance = receivedData[1];
         // mock test
         // const tier = Math.floor(Math.random() * 5);
-        // const pkfBalance = Math.floor(Math.random() * (100000 - 500) + 500);
+        // const tier = 3;
+        // const pkfBalance = Math.floor(Math.random() * (10000 - 500) + 500);
         console.log(`Snapshot user balance with wallet ${wallet} tier ${tier} pkf_balance ${pkfBalance}`);
         // calc lottery_tickets
         // TODO need get setting from Db
@@ -127,6 +128,10 @@ class PickRandomWinnerJob {
       const userSnapshots = await userSnapshotService.getAllSnapshotByFilters(filters);
       const countObj = await userSnapshotService.countByFilters(filters);
       const count = countObj[0]['count(*)'];
+      if (count && count == 0) {
+        console.log(`Do not have any user belong to tier ${tier.level}`);
+        continue;
+      }
       let winners;
       switch (tier.level) {
         case 4:
@@ -145,53 +150,83 @@ class PickRandomWinnerJob {
           });
           break;
         case 3:
-          // calc lottery ticket for tier 3 Eagle
-          let extra_tickets;
-          if (tier.ticket_allow > count) {
-            // pickup random winner for remain tickets
-            extra_tickets = await userSnapshotService.getRandomWinners(tier.ticket_allow - count, tier.level, data.campaign_id);
-          }
-          winners = userSnapshots.toJSON().map(snapshot => {
-            const winnerModel = new WinnerListUserModel();
-            let ticket = 1;
-            if (extra_tickets && PickRandomWinnerJob.checkContain(extra_tickets.toJSON(),snapshot.wallet_address)){
-              ticket ++;
-            }
-            winnerModel.fill({
-              wallet_address: snapshot.wallet_address,
-              campaign_id: data.campaign_id,
-              level: snapshot.level,
-              lottery_ticket: ticket
+          // pickup random lottery ticket for  tier 3 Eagle
+          if (tier.ticket_allow <= count) {
+            // always allocate at least an ticket for each user who has tier 3
+            winners = userSnapshots.toJSON().map((snapshot) => {
+              const winnerModel = new WinnerListUserModel();
+              winnerModel.fill({
+                wallet_address: snapshot.wallet_address,
+                campaign_id: data.campaign_id,
+                level: snapshot.level,
+                lottery_ticket: 1
+              });
+              return winnerModel;
             });
-            return winnerModel;
-          });
+            break;
+          }
+          winners = await PickRandomWinnerJob.pickupRandom(userSnapshots.toJSON(), tier.ticket_allow - count, data.campaign_id, tier.level);
           break;
         default:
           // pickup random lottery ticket for these tiers 1,2 Dove Hawk
-          const randomSnapshot = await userSnapshotService.getRandomWinners(tier.ticket_allow, tier.level, data.campaign_id);
-          winners = randomSnapshot.toJSON().map(snapshot => {
-            const winnerModel = new WinnerListUserModel();
-            winnerModel.fill({
-              wallet_address: snapshot.wallet_address,
-              campaign_id: data.campaign_id,
-              level: snapshot.level,
-              lottery_ticket: 1
-            });
-            return winnerModel;
-          });
+          winners = await PickRandomWinnerJob.pickupRandom(userSnapshots.toJSON(), tier.ticket_allow, data.campaign_id);
       }
       // save to winner list
       await campaignUpdated.winners().saveMany(winners);
     }
   }
 
-  static async checkContain(users,wallet) {
-    for (let i = 0; i < users.length; i++) {
-      if (users.wallet_address == wallet) {
-        return true;
+  static async pickupRandom(userSnapshots, totalWinners, campaign_id, tier) {
+    // map contain winner snapshot
+    let winnerSnapshots = new Map();
+    // convert to array to pick random lottery
+    let userSnapshotArray = [];
+    userSnapshots.map(snapshot => {
+      let i = 1;
+      while (i <= snapshot.lottery_ticket) {
+        userSnapshotArray.push(snapshot);
+        i++;
       }
+      // always push at least one ticket winner for user tier 3
+      if (tier == 3) {
+        snapshot.winner_ticket++;
+        winnerSnapshots.set(snapshot.wallet_address, snapshot);
+      }
+    });
+
+
+    // pick up random winner
+    let i = 1;
+    // loop with max total ticket winner which allocate for each tier
+    while (i <= totalWinners) {
+      const winner = userSnapshotArray[Math.floor(Math.random() * userSnapshotArray.length)];
+      // check existed
+      if (winnerSnapshots.has(winner.wallet_address)) {
+        const existed = winnerSnapshots.get(winner.wallet_address);
+        // increment winner ticket
+        existed.winner_ticket = existed.winner_ticket + 1;
+        winnerSnapshots.set(winner.wallet_address, existed);
+      } else {
+        winner.winner_ticket++;
+        winnerSnapshots.set(winner.wallet_address, winner);
+      }
+      i++;
     }
-    return false;
+
+    // create winner model list
+    const winnerModelList = [];
+    winnerSnapshots.forEach((snapshot) => {
+      const winnerModel = new WinnerListUserModel();
+      winnerModel.fill({
+        wallet_address: snapshot.wallet_address,
+        campaign_id: campaign_id,
+        level: snapshot.level,
+        lottery_ticket: snapshot.winner_ticket
+      });
+      winnerModelList.push(winnerModel);
+    });
+
+    return winnerModelList;
   }
 
   // Dispatch
