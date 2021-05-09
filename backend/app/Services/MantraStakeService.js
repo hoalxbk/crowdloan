2900e0591c467ea5b05900dc4eaa82f620acf528
Nguyen Toan Thang <thang.nguyen2@sotatek.com>
2021-05-09 15:02:46 +0700

    Refactor code Mantra Staking info

--- backend/app/Services/MantraStakeService.js
+++ backend/app/Services/MantraStakeService.js
@@ -38,14 +38,16 @@ class MantraStakeService {
       stakingLog.wallet_address = walletAddress;
       stakingLog.current_tier = (await HelperUtils.getUserTierSmart(walletAddress))[0];
 
-      const tierAmount = await HelperUtils.getUserTotalStakeSmartContract(walletAddress);
-      // const decimals = (await HelperUtils.getExternalTokenSmartContract(walletAddress) || {}).decimals || 18;
-      const decimals = 18;
-      stakingLog.tier_staked_amount = new BigNumber(tierAmount).dividedBy(Math.pow(10, decimals)).toFixed();
-
       const userStaking = await this.findUserStaking(walletAddress);
       stakingLog.user_id = userStaking ? userStaking.id : null;
 
+      // Tier Staked Amount
+      const tierStakedAmount = await HelperUtils.getUserTotalStakeSmartContract(walletAddress);
+      // const decimals = (await HelperUtils.getExternalTokenSmartContract(walletAddress) || {}).decimals || 18;
+      const decimals = 18;
+      stakingLog.tier_staked_amount = new BigNumber(tierStakedAmount).dividedBy(Math.pow(10, decimals)).toFixed();
+
+      // UnStaked Amount (Staked Mantra Dao Smart Contract)
       const unstakeAmountMantra = await HelperUtils.getUnstakeMantraSmartContract(walletAddress) || {};
       stakingLog.mantra_unstake_amount = new BigNumber(unstakeAmountMantra.amount || 0).dividedBy(Math.pow(10, 18)).toFixed();
 
