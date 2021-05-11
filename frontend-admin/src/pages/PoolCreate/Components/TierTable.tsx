8c2a75f7175170ff1eacb3167149399973545cdc
Nguyen Toan Thang <thang.nguyen2@sotatek.com>
2021-05-11 10:40:48 +0700

    Fix show time 1970 with Tier level 0

--- frontend-admin/src/pages/PoolCreate/Components/TierTable.tsx
+++ frontend-admin/src/pages/PoolCreate/Components/TierTable.tsx
@@ -54,10 +54,8 @@ function TierTable(props: any) {
       const dataFormatted = poolDetail.tiers.map((item: any, index: any) => {
         return createData(
           TIERS[index],
-          // moment(moment.utc(item.start_time * 1000)).format(DATETIME_FORMAT),
-          // moment(moment.utc(item.end_time * 1000)).format(DATETIME_FORMAT),
-          moment(item.start_time * 1000).format(DATETIME_FORMAT),
-          moment(item.end_time * 1000).format(DATETIME_FORMAT),
+          item.start_time ? moment(item.start_time * 1000).format(DATETIME_FORMAT) : null,
+          item.end_time ? moment(item.end_time * 1000).format(DATETIME_FORMAT) : null,
           (new BigNumber(item.min_buy)).toNumber(),
           (new BigNumber(item.max_buy)).toNumber(),
           false,
