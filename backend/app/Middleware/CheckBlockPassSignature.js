5a587c1c013463e06d4a53b76f764c5804cf8efe
Nguyen_Viet_Hung <hung.nguyen4@sotatek.com>
2021-05-26 11:44:51 +0700

    Fix bug call multiple times hash.digest() per hash instance

--- backend/app/Middleware/CheckBlockPassSignature.js
+++ backend/app/Middleware/CheckBlockPassSignature.js
@@ -3,7 +3,6 @@
 const ErrorFactory = use('App/Common/ErrorFactory');
 const crypto = require('crypto');
 const BLOCK_PASS_SECRET_KEY = process.env.BLOCK_PASS_SECRET_KEY;
-const crypt = crypto.createHmac('sha256', BLOCK_PASS_SECRET_KEY);
 
 class CheckBlockPassSignature {
   async handle({request}, next) {
@@ -11,7 +10,7 @@ class CheckBlockPassSignature {
       const body = request.post();
       // console.log(JSON.stringify(body))
       const signature = request.headers()['x-hub-signature'];
-      const hash = crypt.update(JSON.stringify(body)).digest("hex");
+      const hash = crypto.createHmac('sha256', BLOCK_PASS_SECRET_KEY).update(JSON.stringify(body)).digest("hex");
       console.log(`Check header of block pass with signature ${signature} and hash ${hash}`);
       if (!signature || signature !== hash) {
         console.log('Access denied !');
