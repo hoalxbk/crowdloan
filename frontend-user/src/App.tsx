edbde190db5865f3ff0e2b37977ae18052018f2d
Nguyen Toan Thang <thang.nguyen2@sotatek.com>
2021-06-04 17:32:58 +0700

    [Hotfix] - [Prod] - Add version

--- frontend-user/src/App.tsx
+++ frontend-user/src/App.tsx
@@ -8,6 +8,8 @@ import configureStore from './store/configureStore'
 import { ThemeProvider } from '@material-ui/core/styles';
 import defaultTheme from './themes/DefaultTheme/DefaultTheme';
 
+console.log('Version: ', process.env.TEMP_NFT_PLUS_AMOUNT_PRODUCTION || 0);
+
 export const getLibrary = (provider: any): Web3Provider => {
   const library = new ethers.providers.Web3Provider(provider, 'any');
   library.pollingInterval = 10000;
